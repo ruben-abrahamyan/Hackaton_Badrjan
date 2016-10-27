d0d0b8f8f336cab099062c99034121a38dff6fd9
Ruben Abrahamyan <rubenabrahamyan@RubenAbrahamyanMM.local>
2016-10-27 16:02:38 +0400

    asd

---
+++ Badrjan_Hackaton/Badrjan_Hackaton/BDCollectionViewCell.m
@@ -0,0 +1,18 @@
+//
+//  BDCollectionViewCell.m
+//  Badrjan_Hackaton
+//
+//  Created by Ruben Abrahamyan on 10/27/16.
+//  Copyright © 2016 Badrjan. All rights reserved.
+//
+
+#import "BDCollectionViewCell.h"
+
+@implementation BDCollectionViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
