fd46fb43c91937a8ee5756c1c7d84cdba910294b
gevorgsukiasyan <gevorg.sukiasyan@picsart.com>
2016-10-27 13:03:05 +0400

    user demo

---
+++ Badrjan_Hackaton/Badrjan_Hackaton/BDUser.h
@@ -0,0 +1,33 @@
+//
+//  BDUser.h
+//  Badrjan_Hackaton
+//
+//  Created by Gevorg Sukiasyan on 10/27/16.
+//  Copyright © 2016 Badrjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface BDUser : NSObject
+
+@property (strong, nonatomic) NSString *username;
+
+@property (strong, nonatomic) NSMutableArray *preferredStyle;
+
+@property (strong, nonatomic) UIImage *thumbnail;
+
+@property (strong, nonatomic) UIImage *targetPhoto;
+
+@property (strong, nonatomic) NSMutableArray *topMoods;
+
+@property (strong, nonatomic) NSMutableArray *currentPlaylist;
+
+@property (strong, nonatomic) NSMutableArray *receivedSongs;
+
+@property (strong, nonatomic) NSString *chosenMood;
+
+
+
+
+@end
