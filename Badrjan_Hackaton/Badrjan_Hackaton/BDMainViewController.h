0a6221a5d4d5dcb34259d9ee69da2e28238e0475
Ruben Abrahamyan <rubenabrahamyan@rubenabrahamyanmm.local>
2016-10-27 14:10:37 +0400

    commits

---
+++ Badrjan_Hackaton/Badrjan_Hackaton/BDMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  BDMainViewController.h
+//  Badrjan_Hackaton
+//
+//  Created by Ruben Abrahamyan on 10/27/16.
+//  Copyright © 2016 Badrjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDMainViewController : UIViewController
+
+@end
