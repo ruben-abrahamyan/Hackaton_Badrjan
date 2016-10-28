c0b135f51c49b7d1aa381c8b33e35efdb6aec9c4
gevorgsukiasyan <gevorg.sukiasyan@picsart.com>
2016-10-28 05:22:22 +0400

    finally

---
+++ Badrjan_Hackaton/Badrjan_Hackaton/BDMoodTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BDMoodTableViewCell.h
+//  Badrjan_Hackaton
+//
+//  Created by Gevorg Sukiasyan on 10/28/16.
+//  Copyright © 2016 Badrjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BDMoodTableViewCell : UITableViewCell
+
+@end
---
+++ Badrjan_Hackaton/Badrjan_Hackaton/BDMoodTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  BDMoodTableViewCell.m
+//  Badrjan_Hackaton
+//
+//  Created by Gevorg Sukiasyan on 10/28/16.
+//  Copyright © 2016 Badrjan. All rights reserved.
+//
+
+#import "BDMoodTableViewCell.h"
+
+@implementation BDMoodTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
